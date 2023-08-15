377f8909ec4a44b5fdfcc9eb500462dd352191e0
Raphaella-leite <77009283+Raphaella-leite@users.noreply.github.com>
2023-08-15 09:32:56 -0300

    Calculadora do Louzada
    Nas suas férias de verão, nosso querido Louzada estava jogando videogame tranquilamente em sua casa. Tudo estava maravilhoso para nosso protagonista. Entretanto, aos 45 do segundo tempo, para a tristeza do nosso jogador, a sua entrega do iFood havia chegado na sua casa e ele precisou sair correndo para atender o motoboy. Já não bastava o Louzada ter que abandonar a partida de FIFA, mas ele também precisou ajudar o motoboy que, infelizmente, havia esquecido a sua calculadora no restaurante, com os cálculos do seu pedido. 
    
    ​
    
    Para não passar por esse perrengue novamente de ter que ajudar motoboys esquecidos, ele resolveu criar um programa com a linguagem Python que funcionaria como uma calculadora. O programa precisa fazer 3 coisas:
    
    Printe: "Calculadora do Louzada"
    
    Pergunte o preço dos três produtos do pedido através do input
    
    Calcule o preço total dos pedido
    
    Mostre para o motoboy o preço total da compra no iFood
    
    ​
    
    (Insira os preços que você preferir na hora de testar a calculadora)

---
+++ Calculadora do Louzada.py
@@ -0,0 +1,9 @@
+print ('Calculadora do Louzada')
+
+preco1 = float(input("Digite o preço do produto 1:"))
+preco2 = float(input("Digite o preço do produto 2:"))
+preco3 = float(input("Digite o preço do produto 3:"))
+
+valor_total= (preco1 + preco2 + preco3)
+
+print (f'O valor total é R$: {valor_total}')
