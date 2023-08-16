f775013097d709f00ea3bdc9f14374be1163dfe2
Raphaella-leite <77009283+Raphaella-leite@users.noreply.github.com>
2023-08-16 09:48:47 -0300

    Add files via upload

---
+++ CALCULOS DE GEOMETRIA.py
@@ -0,0 +1,20 @@
+print('=====CALCULOS DE GEOMETRIA======')
+
+x = 3
+y = 5
+
+area_cir = (3 *(3**2))
+
+area_trap = ((3 + 5) * 4)
+
+print(f'A área do quadrado de lado x é: {x * 4}')
+
+print(f'O perímetro de um triângulo de base x e lados y é : {x + y + y}')
+
+print(f'A área de um círculo de raio x (π = 3) é :{area_cir} ')
+
+print(f'A área de um trapézio com B=x, b=y e altura = 4 é :{area_trap} ')
+
+
+
+
