c9b75e95e903ece8ec68bdba740ea302e70e5e4b
Raphaella-leite <77009283+Raphaella-leite@users.noreply.github.com>
2023-08-16 09:50:17 -0300

    Add files via upload

---
+++ Teorema de pitagoras.py
@@ -0,0 +1,12 @@
+print("=======TEOREMA DE PITÁGORAS========")
+print("a² = b² + c²")
+
+b = 9
+c = 12
+
+calculo_a_b = (9**2) + (12**2)
+
+hipotenusa = calculo_a_b **(1/2)
+
+print(f'O valor da hipotenusa é: {hipotenusa} m')
+
